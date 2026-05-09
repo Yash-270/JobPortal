f9d7f9a80e576f9ad8a7106fd1d02bb7500ac65a
Yash Kumar <kyash4634@gmail.com>
2026-05-09 17:04:45 +0530

    Fix formatting of lowercase property in user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         required: true,
         unique: true,
-        lowercase: true, // 👈 AUTO convert
+        lowercase: true,
         trim: true
     },
     password:{
