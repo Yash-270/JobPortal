91d0610025d4af874ae36df62d398d3d3561711c
Yash Kumar <kyash4634@gmail.com>
2026-05-09 17:02:31 +0530

    Clean up adminRoute.js by removing unused comments
    Removed commented-out code for applications and status count.

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -14,19 +14,14 @@ router.get("/dashboard",jwtAutMidd,async(req,res)=>{
         }
         const totalJobs = await Job.countDocuments(filter);
         const activeJobs = await Job.countDocuments({...filter,isActive: true });
-        //const totalApplications = await Result.countDocuments();
 
-        // const statusCount = await Result.aggregate([
-        // { $group: { _id: "$status", count: { $sum: 1 } } }
-        // ]);
-
-        // 🔥 RECENT 5 JOBS
+       
         const recentJobs = await Job.find(filter)
         .sort({ createdAt: -1 })
         .limit(5)
         .select("title company jobType salary createdAt");
 
-        // 🔥 TOP JOBS (most applied)
+
         const topJobs = await Job.find(filter)
         .sort({ jobCount: -1 })
         .limit(5)
