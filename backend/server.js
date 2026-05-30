df1457d05d2cd0f32286338db6ce8d44ff4966ec
Yash Kumar <j>
2026-05-30 10:41:25 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,8 @@ const db=require("./db");
 const app=express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: true,
+  credentials: true
 }));
 
 app.use(express.json());//bdoy parser
